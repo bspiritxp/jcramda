f27b76dbc92daa801e7d454dced4740efc9451a8
Jochen.He <thjl@hotmail.com>
2020-07-30 09:36:32 +0800

    add

--- jcramda/core/_curry.py
+++ jcramda/core/_curry.py
@@ -8,19 +8,17 @@ def has_default_value(p: Parameter):
     return p.default != Parameter.empty
 
 
-def has_holder(fn, pos: int = None):
+def count_holder(fn):
     if isinstance(fn, partial) and len(fn.args) > 0:
-        assert pos is None or [pos, abs(pos) - 1][pos < 0] < len(fn.args), \
-            'checked position is out of bound'
-        return _ in fn.args if pos is None else fn.args[pos] == _
-    return False
+        return len(list(filter(lambda x: x == _, fn.args)))
+    return 0
 
 
 def update_args(fn, args):
     arg_values = list(args)
     new_args = []
     func = fn
-    if has_holder(fn):
+    if count_holder(fn):
         assert len(args), 'when _ in args, must pass a value to fill it'
         for a in fn.args:
             v = a
@@ -31,27 +29,51 @@ def update_args(fn, args):
     return partial(func, *arg_values)
 
 
+def is_filled(fn, spec):
+    if bool(spec.varargs):
+        return False
+    filled_args = ()
+    func = fn
+    while isinstance(func, partial):
+        filled_args += func.args
+        for key in func.keywords:
+            if key in spec.args:
+                filled_args += (func.keywords[key],)
+        func = func.func
+    return [0, len(spec.args)][bool(spec.args)] == len(filled_args) and not count_holder(fn)
+
+
+# def move_kw_to_pos(spec, args, kwargs):
+#     params = list(args)
+#     if spec.args:
+#         for index, key in enumerate(spec.args):
+#             if key in kwargs:
+#                 fix_count = index - len(params) - 1
+#     return args
+
+
 def curry(fn):
     @wraps(fn.func if hasattr(fn, 'func') else fn)
     def curried(*args, **kwargs):
         spec = getfullargspec(fn.func if hasattr(fn, 'func') else fn)
+        # args = move_kw_to_pos(spec, args, kwargs)
         has_args = bool(spec.args) or bool(spec.varargs)
-        has_kws = bool(spec.kwonlyargs) or bool(spec.varkw)
-        if has_args and len(args) + len(kwargs) == 0:
-            return fn(*args, **kwargs)
-        if has_kws and len(kwargs) == 0:
-            return fn(**kwargs)
-        last_is_holder = has_holder(fn, -1)
-        updated_fn = update_args(fn, args) if has_args and len(
-            args) else fn
+        # has_kw = bool(spec.kwonlyargs) or bool(spec.varkw)
+        holders = count_holder(fn)
+        updated_fn = update_args(fn, args) if has_args else fn
         updated_fn = partial(updated_fn, **kwargs) if len(kwargs) else updated_fn
         un_fill_params = list(signature(updated_fn).parameters.values())
 
-        direct_run = not has_holder(updated_fn) and (
-                len(un_fill_params) == 0 or all(map(has_default_value, un_fill_params))
-                or last_is_holder and all(map(lambda p: p.kind in (
-                    Parameter.VAR_POSITIONAL, Parameter.VAR_KEYWORD), un_fill_params))
-        )
+        direct_run = not count_holder(updated_fn) and any([
+            # 没有需要填充的参数
+            len(un_fill_params) == 0,
+            not bool(spec.kwonlyargs) and is_filled(updated_fn, spec),
+            # 所有未填充的参数都有默认值，且此次调用未传入参数
+            all(map(has_default_value, un_fill_params)) and len(args) - holders <= 0,
+            # 带占位符或位置参数都已填充时，剩余参数 ALL IS 动态参数
+            holders and all(map(lambda p: p.kind in (
+                Parameter.VAR_POSITIONAL, Parameter.VAR_KEYWORD), un_fill_params))
+        ])
         if direct_run:
             return updated_fn()
         return curry(updated_fn)
---
+++ jcramda/core/test_curry.py
@@ -0,0 +1,47 @@
+from ._curry import curry, _
+
+
+@curry
+def c1(a: int, b, c=1):
+    return a, b, c
+
+
+@curry
+def c2(a, *args):
+    r = (a,) + args
+    return r
+
+
+@curry
+def c3(a, **kwargs):
+    return a, kwargs
+
+
+@curry
+def c4(* , a=1, b=3, c):
+    return a, b, c
+
+
+def test_curry_args():
+    assert c1(7, 3, _)(6) \
+           == c1(7)(3)(6) \
+           == c1(_, 3, 6)(7) \
+           == c1(7, 3)(6) \
+           == c1(7)(3, 6) \
+           == c1(7, _, 6)(3) \
+           == c1(7)(_, 6)(3) \
+           == (7, 3, 6)
+    assert c1(7, _)(3) == (7, 3, 1)
+    assert c1(c=6)(7, 3) == (7, 3, 6)
+    assert (7, 3, 6) \
+        == c2(7, 3, _)(6) \
+        == c2(_, 3)(7, 6) \
+        == c2(7, _, 6)(3) \
+        == c2(7)(3, _)(6) \
+        == c2(7)(_, 6)(3)
+
+
+def test_curry_kw():
+    assert (1, {'c': 3}) == c3(_, c=3)(1)
+    assert c3(3, b=1) == c3(b=1)(3) == (3, {'b': 1})
+    assert c4(a=3)(c=5) == (3, 3, 5)
